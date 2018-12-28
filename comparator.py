312c9f5f87a11c84a3be6a1ab515187f67b295dc
shubham <4dman.com@gmail.com>
2018-12-29 01:18:38 +0530

    Add main.py and comparator.py
    Everything is working! I am aware of a few bugs and a few necessary features here and there, but it is working and usable. In the future, make testcase_generator smarter by specifically generating edge cases and so on.

---
+++ comparator.py
@@ -0,0 +1,25 @@
+def compare(thepath,args):
+	'''takes a directory and a list of files in it and finds points of differences'''
+	f = [None]*len(args)
+	for i in range(len(args)):
+		theoutput = args[i]
+		print(args[i])
+		with open(thepath+"/"+theoutput) as l:
+			f[i] = l.read().split("\n")
+
+	lengths = [len(i) for i in f]
+	if len(set(lengths)) == 1:
+		print("Good News, the output files all have equal number of files. At least. Phew!")
+	else:
+		print("Well, not all putput files have same number of lines. Please open the text files and see.")
+
+	l = min(lengths)
+	nd = 0 #number of points of difference
+	for j in range(l):
+		if nd>3:
+			print("At least three points of difference have been reported. First address those please. ")
+		g = [f[i][j] for i in range(len(args))]
+		if len(set(g))!=1:
+			nd+=1
+			print(f"A point of difference found in sentence {j+1}.")
+			
---
+++ main.py
@@ -0,0 +1,61 @@
+#make a function comparator too
+#be smart in testcase generation. check edgecases, random etc.
+#should handle tle too
+
+def main():
+	import os
+
+	projectdirpath = input("Enter project directory or simply press Enter if it's in current directory: ") or '.'
+	
+	projectdir = os.listdir(projectdirpath) 
+	files = [f for f in projectdir if os.path.isfile(f) if f[:4]=="code"]
+	if len(files)>=2:
+		print(f"Detected {len(files)} files {files} to compare. Great!")
+
+	else:
+		f0 = input("Enter the path of the first code file: ") 
+		f1 = input("Enter the path of the second code file: ")
+		files = [f0, f1]
+	if "thecases.txt" in projectdir:
+		print("Using the 'thecases.txt' as testcases. If you don't want that, remove it and rerun this script.")
+		ipfile = None
+
+	elif "thein.txt" in projectdir:
+		ipfile = input("Detected 'thein.txt'. Please make sure that it is the correct input-format file and press Enter. Alternatively, type the path to input-format file: ") or projectdirpath+"/"+"thein.txt"
+
+	else:
+		from template import buildtemplate
+		buildtemplate(projectdirpath)
+		ipfile = input("Kindly edit the newly generated 'thein.txt' in your project directory and press Enter here or input the location of input format file: ") or projectdirpath+"/"+"thein.txt"
+
+	testcases = projectdirpath+"/thecases.txt"
+	# if testcases[0]==testcases[-1]=='"':
+	# 	testcases=testcases[1:-1]
+	condn = None#condn =  input("Enter constraints(if any) imposed on the testcases: ")
+
+	if ipfile!=None:
+		if ipfile[0]==ipfile[-1]=='"':
+			ipfile=ipfile[1:-1]
+		from testcase_generator import driver 
+		driver(ipfile, testcases, condn)
+	
+	for thefile in files:
+		lang = thefile[::-1][:thefile[::-1].index(".")][::-1]
+		print(thefile)
+			
+		if lang == "cpp" or lang == "c":
+			com = None
+			if lang=="cpp":
+				com = "g++"
+			elif lang == "c":
+				com = "gcc"
+			os.system(f"{com} -o temporaryexecutable {thefile} && temporaryexecutable <{testcases}> out{thefile[:-len(lang)]}txt &&del temporaryexecutable.exe")
+		elif lang == "py":
+			com = "py"
+			os.system(f"{com} {thefile} <{testcases}> out{thefile[:-len(lang)]}txt")
+
+	from comparator import compare
+	compare(projectdirpath, ["out"+thefile[:-len(  thefile[::-1][:thefile[::-1].index(".")][::-1]  )]+"txt" for thefile in files])
+
+if __name__=="__main__":
+	main()
