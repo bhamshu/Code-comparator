32326a6988e72a45ee0bc9e5ec6127eff20e1ba3
shubham <4dman.com@gmail.com>
2019-01-02 17:00:53 +0530

    Added compare multiple files to a solution feature
    The name of the solution code must be "code.ext" where 'ext' belongs to
    {'py', 'java', 'c', 'cpp'}. If such a file is detected, the outputs of
    all other codes will be compared to the output of this code.

--- comparator.py
+++ comparator.py
@@ -1,4 +1,4 @@
-def compare(thepath, args):
+def compare(thepath, args, sol = None):
 	'''takes a directory and a list of files in it and finds points of differences'''
 	f = [None]*len(args)
 	for i in range(len(args)):
@@ -7,24 +7,44 @@ def compare(thepath, args):
 		with open(thepath+"/"+theoutput) as l:
 			f[i] = l.read().split("\n")
 
-	lengths = [len(i) for i in f]
-	if len(set(lengths)) == 1:
-		eqlen = True
+	if sol!=None:
+		with open(thepath+"/"+sol) as l:
+			thesol = l.read().split("\n")
+		ds = []
+		for i in f:
+			if len(i) == len(thesol):
+				eqlen = True
+			else:
+				eqlen = False
+
+			l = min(len(i), len(thesol))
+			nd = 0 #number of points of difference
+			ls = []
+			for j in range(l):
+				if i[j]==thesol[j]:
+					nd+=1
+					ls.append(j)
+
+			ds.append( (nd, ls, eqlen) )
+		return ds
+
 	else:
-		eqlen = False
+		lengths = [len(i) for i in f]
+		if len(set(lengths)) == 1:
+			eqlen = True
+		else:
+			eqlen = False
 
-	l = min(lengths)
-	nd = 0 #number of points of difference
-	ls = []
-	for j in range(l):
-		g = [f[i][j] for i in range(len(args))]
-		if len(set(g))!=1:
-			nd+=1
-			ls.append(j)
+		l = min(lengths)
+		nd = 0 #number of points of difference
+		ls = []
+		for j in range(l):
+			g = [f[i][j] for i in range(len(args))]
+			if len(set(g))!=1:
+				nd+=1
+				ls.append(j)
 
-	return nd, ls, eqlen
-
-
+		return [(nd, ls, eqlen)]
 
 def main():
 	thepath = input("Enter the directory in which the files are located or press Enter if it's current directory: ") or "."
--- main.py
+++ main.py
@@ -56,31 +56,48 @@ def main():
 			built = f"{com} -o temporaryexecutable {thefile} && "
 			if "temporaryexecutable.exe" in os.listdir(projectdirpath):
 				built = ""
-			os.system(built+f"temporaryexecutable <{testcases}> out{thefile[:-len(lang)]}txt"+delet)
+			os.system(built+f"temporaryexecutable <{testcases}> \"out{thefile[:-len(lang)]}txt\" "+delet)
 		elif lang == "py":
 			com = "py"
-			os.system(f"{com} {thefile} <{testcases}> out{thefile[:-len(lang)]}txt")
+			print(f"{com} \"{thefile}\" <\"{testcases}\"> \"out{thefile[:-len(lang)]}txt\"")
+			os.system(f"{com} \"{thefile}\" <\"{testcases}\"> \"out{thefile[:-len(lang)]}txt\"")
 		elif lang == "java":
 			com = "javac"
 			#print(f"{com} {thefile} & java {thefile[:-len(lang)-1]} <{testcases}> out{thefile[:-len(lang)]}txt")
-			os.system(f"{com} {thefile} & java {thefile[:-len(lang)-1]} <{testcases}> out{thefile[:-len(lang)]}txt")
+			os.system(f"{com} \"{thefile}\" & java \"{thefile[:-len(lang)-1]}\" <\"{testcases}\"> \"out{thefile[:-len(lang)]}txt\" ")
 			def deleteclassfiles():
 				# print(projectdir, projectdir[3][-5:])
 				# cfil = [f for f in projectdir if os.path.isfile(f) if f[-5:]=="class"]
 				# print(cfil)
 				# for i in cfil:
 					# os.system(f"del {i}")
-				os.system(f"del {thefile[:-len(lang)]}class & del {thefile[:-len(lang)-1]}$FastReader.class")
+				os.system(f"del \"{thefile[:-len(lang)]}class\" & del \"{thefile[:-len(lang)-1]}$FastReader.class\"")
 			deleteclassfiles()
 
 	from comparator import compare
-	nd, ls, eqlen = compare(projectdirpath, ["out"+thefile[:-len(  thefile[::-1][:thefile[::-1].index(".")][::-1]  )]+"txt" for thefile in files])
-	if nd == 0:
-		print("Congrats, all the files match. However, that doesn't necessarily means that the codes will always produce same output for same input. Run again for more confidence.")
+	theoutfiles = ["out"+thefile[:-len(  thefile[::-1][:thefile[::-1].index(".")][::-1]  )]+"txt" for thefile in files]
+	if "outcode.txt" in os.listdir(projectdirpath):
+		ds = compare(projectdirpath, theoutfiles, sol = "outcode.txt")
+		for l in range(len(ds)):
+			if theoutfiles[l]=="outcode.txt":
+				continue
+			nd, ls, eqlen = ds[l]
+			print("")
+			if nd == 0:
+				print(f"Congrats, {theoutfiles[l]} matches with outcode.txt. However, that doesn't necessarily means that the codes will always produce same output for same input. Run again for more confidence.")
+			else:
+				print(f"Points of difference between {theoutfiles[l]} and outcode.txt found in statements {[i+1 for i in ls]}.")
+			print(f"Total number of differences = {len(ls)}")
+			if not eqlen:
+				print("Note that the two files do not have same number of lines.")
 	else:
-		print(f"Points of difference found in statements {[i+1 for i in ls]}.")
-	if not eqlen:
-		print("Not all output files have same number of lines.")
+		nd, ls, eqlen = compare(projectdirpath, theoutfiles)[0]
+		if nd == 0:
+			print("Congrats, all the files match. However, that doesn't necessarily means that the codes will always produce same output for same input. Run again for more confidence.")
+		else:
+			print(f"Points of difference found in statements {[i+1 for i in ls]}.")
+		if not eqlen:
+			print("Not all output files have same number of lines.")
 
 if __name__=="__main__":
 	main()
